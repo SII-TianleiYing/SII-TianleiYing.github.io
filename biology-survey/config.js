bc9a5c697581140c50209f14c1bfa5d60114a280
Feng Yi <yfeng6918@gmail.com>
2026-01-21 22:36:36 +0800

    Create config.js

---
+++ biology-survey/config.js
@@ -0,0 +1,6 @@
+// 把你部署 Apps Script 后得到的 Web App URL 粘到这里（以 /exec 结尾）
+// 例：https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+window.SURVEY_CONFIG = {
+  APPS_SCRIPT_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzQMjq0b9P8pt5uqUFMpKVsvclFcHnBMCKVrJwm-CrfswWZpKnY8W3UN80TKIoRjVc1tQ/exec",
+};
+
